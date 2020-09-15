5d160e8fdcd668e7c48240ee26b995faf8c0ad6e
Walid Moustafa <walid.moustafa@unico.com.au>
2020-09-15 16:28:53 +1000

    Initial commit

---
+++ src/main/java/com/walid/gw/TwitterGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.walid.gw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wmoustafa
+ */
+@SpringBootApplication
+public class TwitterGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/walid/gw/TwitterGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.walid.gw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author wmoustafa
+ */
+@SpringBootTest
+class TwitterGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
