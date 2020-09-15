8956c5e29ea2cddfbb77ed5f08b810ce4e564823
Walid Moustafa <walid.moustafa@unico.com.au>
2020-09-15 20:58:03 +1000

    List dummy results on home page

--- src/main/java/com/walid/gw/controller/SearchController.java
+++ src/main/java/com/walid/gw/controller/SearchController.java
@@ -1,8 +1,12 @@
 package com.walid.gw.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,8 +21,13 @@ public class SearchController {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @GetMapping()
-    public String home(@RequestParam(value = "keywords", defaultValue = "java") String keywords) {
+    public String home(@RequestParam(value = "keywords", defaultValue = "java") String keywords,
+                       Model model) {
         logger.debug("Received a search for keywords \"{}\"", keywords);
+        List<String> tweets = new ArrayList<>();
+        tweets.add("Java is cool");
+        tweets.add("Java is old");
+        model.addAttribute("tweets", tweets);
         return "home";
     }
 }
